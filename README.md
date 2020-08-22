95d660930230ba4d36e4510ccafedb17f7bb74c1
Jeremy Deutsch <jeremy.e.deutsch@gmail.com>
2020-08-22 16:51:01 -0700

    set up repro for DLL error

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-native-windows DLL error repro
+
+Instructions for starting the app: https://microsoft.github.io/react-native-windows/docs/getting-started#running-a-react-native-windows-app
---
+++ windows/rnw_dll_repro/LaunchApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.ReactNative.Managed;
+
+namespace rnw_dll_repro
+{
+    [ReactModule]
+    class LaunchApp
+    {
+        [ReactMethod("launchApp")]
+        public void Launch(string path)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(path);
+            Process.Start(startInfo);
+        }
+    }
+}
