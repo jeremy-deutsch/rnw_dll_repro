159970feb69d98d3f739a3d0ed2f2feba4b6267b
Jeremy Deutsch <jeremy.e.deutsch@gmail.com>
2020-08-22 16:31:23 -0700

    initial commit from template

---
+++ windows/rnw_dll_repro/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ReactNative;
+
+namespace rnw_dll_repro
+{
+    sealed partial class App : ReactApplication
+    {
+        public App()
+        {
+            MainComponentName = "rnw_dll_repro";
+
+#if BUNDLE
+            JavaScriptBundleFile = "index.windows";
+            InstanceSettings.UseWebDebugger = false;
+            InstanceSettings.UseFastRefresh = false;
+#else
+            JavaScriptMainModuleName = "index";
+            InstanceSettings.UseWebDebugger = true;
+            InstanceSettings.UseFastRefresh = true;
+#endif
+
+#if DEBUG
+            InstanceSettings.EnableDeveloperMenu = true;
+#else
+            InstanceSettings.EnableDeveloperMenu = false;
+#endif
+
+            Microsoft.ReactNative.Managed.AutolinkedNativeModules.RegisterAutolinkedNativeModulePackages(PackageProviders); // Includes any autolinked modules
+            
+            PackageProviders.Add(new Microsoft.ReactNative.Managed.ReactPackageProvider()); // Includes any modules in this project
+
+            InitializeComponent();
+        }
+    }
+}
